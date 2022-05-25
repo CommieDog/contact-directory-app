3c7f86f643082e06015fc594d262a4286c79ae03
Hexuan Li <lihexuan1@gmail.com>
2022-05-25 10:44:08 -0700

    update webpack.config.js file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = () => {
   return {
@@ -11,17 +14,54 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+     new HtmlWebpackPlugin({
+       template: './index.html',
+       title: 'Contact Directory App'
+     }),
+    new GenerateSW(),
+    new WebpackPwaManifest({
+      name: 'Contact Directory App',
+      short_name: 'Contacts',
+      description: 'Find your contact',
+      background_color: 'white',
+      theme_color: 'white',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('src', 'icons'),
+        },
+      ],
+    })
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ]
     }
   };
 };
