67e8e9c9f6eb8234d2e2f6bcb82befe5a7dd813f
John Netzel <johnrnetzel@gmail.com>
2022-05-25 10:58:02 -0800

    Fix service worker read filename

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -4,8 +4,9 @@ import '../css/styles.css';
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // Register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+  const workboxSW = new Workbox('/service-worker.js');
   workboxSW.register();
+  console.log("Service Worker registered!");
 } else {
   console.error('Service workers are not supported in this browser.');
 }
